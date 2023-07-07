665255e6c962f72fc69d137cb067be681da9b2ba
Jafar Hussein <Jafarhussein6@gmail.com>
2023-07-07 16:26:14 +0200

    Första java app

---
+++ app/src/main/java/com/example/greetingapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.greetingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
+    TextView textView;
+    EditText editText;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override // this is called when the button is clicked
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                Toast.makeText(MainActivity.this, "Hello "+name, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
